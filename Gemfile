7320db3d3e77e99c17479eeb803e2ba87e12bd33
Jose Simoes <simoes83@gmail.com>
2011-10-23 20:11:14 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+
+group :development do
+  gem 'rspec-rails'
+end
+
+
+group :test do
+  gem 'rspec'
+end
+
+gem 'sqlite3'
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '54a84b5c076742a885e1ff01164536f7a332c83a5e74f4104cb106e33d6bddcc1edec0b80407cd99dd2672c0a97b800e4b4964269930829e0acb53698c9787b8'
